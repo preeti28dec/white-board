7b298cd5018dad29ffdaa6c7af87b5b85f2d39c2
preeti28dec <preetithakur28dec2000@gmail.com>
2022-12-19 10:37:23 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./container/login";
 import { useEffect, useState } from "react";
 import DashBoard from "./container/dashboard";
 import Signup from "./container/singup";
 function App() {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(false);
+  let navigate = useNavigate();
   useEffect(() => {
-    const auth = localStorage.getItem("userRegister");
-    const user = !!auth ? JSON.parse(auth) : undefined;
-    setUser(user);
-  }, []);
-  console.log(user);
+    const auth = JSON.parse(localStorage.getItem("userRegister"));
+    if (auth) {
+      setUser(true);
+      navigate('/');
+    }
+    else{
+      setUser(false)
+      navigate('/login')
+    }
+  },[user]);
 
   return (
     <>
       <Routes>
-        {user ? (
-          <Route path="/" element={<DashBoard />} />
-        ) : (
-          <Route path="/login" element={<Login />} />
-        )}
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
